b84e05d585454367595267be28a0ce226911001b
Flogard <eirik.flogard@arbeidstilsynet.no>
2023-03-07 09:42:10 +0100

    cleaned up some files

--- mycbr-rest-master IJCAI/example/mycbrwrapper/myCBRUtil.py
+++
@@ -1,9 +0,0 @@
-from mycbrwrapper.rest import getRequest
-
-def clearMyCBR(host):
-    api = getRequest(host)
-    call = api.concepts
-    result = call.GET()
-    concepts = result.json().get("concept")
-    for elem in concepts:
-        api.concepts(elem).DELETE()
